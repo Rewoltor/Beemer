0b94ca41f358c05473cf5db79b5c1bc005931c56
Rewoltor <baltay.marton@gmail.com>
2021-12-30 14:55:56 +0100

    sd

--- dbh.inc.php
+++ dbh.inc.php
@@ -1,21 +1,21 @@
 <?php
 
-
+/*
 $serverName = "localhost:3307";
 $dBUsername = "root";
 $dBPassword = "";
 $dBName = "beemer1"; //name of mysql database xampp
 
 
-/*
 
+*/
 
 $serverName = "eu-cdbr-west-01.cleardb.com";
 $dBUsername = "bfef03bc6baad8";
 $dBPassword = "d8944d55";
 $dBName = "heroku_87332ee8d6e83a2"; //name of mysql database xampp
 
-*/
+
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
